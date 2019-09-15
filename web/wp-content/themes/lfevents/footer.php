716f1400710c4aceded49d5c60bc0daa98af19da
Andy Cochran <acochran@planning.nyc.gov>
2019-09-15 16:01:09 -0400

    strip FoundationPress

--- web/wp-content/themes/lfevents/footer.php
+++ web/wp-content/themes/lfevents/footer.php
@@ -2,8 +2,6 @@
 /**
  * The template for displaying the footer
  *
- * Contains the closing of the "off-canvas-wrap" div and all content after.
- *
  * @package FoundationPress
  * @since FoundationPress 1.0.0
  */
--- web/wp-content/themes/lfevents/functions.php
+++ web/wp-content/themes/lfevents/functions.php
@@ -26,10 +26,6 @@ require_once 'library/class-foundationpress-comments.php';
 /** Register all navigation menus */
 require_once 'library/navigation.php';
 
-/** Add menu walkers for top-bar and off-canvas */
-require_once 'library/class-foundationpress-top-bar-walker.php';
-require_once 'library/class-foundationpress-mobile-walker.php';
-
 /** Create widget areas in sidebar and footer */
 require_once 'library/widget-areas.php';
 
